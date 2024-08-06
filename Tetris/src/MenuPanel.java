6f730599ac95021317766a4adea9523e0b0a7fee
Shalev <shalev3d@gmail.com>
2024-08-06 20:51:43 +0300

    Commitint

--- Tetris/src/MenuPanel.java
+++ Tetris/src/MenuPanel.java
@@ -1,9 +1,9 @@
 
-public class main {
+public class MenuPanel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("HELLO IT WORKS");
 	}
 
 }
